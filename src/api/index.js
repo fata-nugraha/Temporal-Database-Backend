c8a3898880a11ca48f85b5a995fa925369f7df9d
Ft-N <ftnfata@gmail.com>
2020-11-25 08:17:51 +0700

    Test redeploy

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const router = new Router()
 module.exports = router
 
 router.get('/api/test', async (req, res) => {
-  const query = 'SELECT * FROM visitor WHERE checkin_date<$1 LIMIT 10;';
+  const query = 'SELECT * FROM visitor WHERE checkin_date<$1 LIMIT 10';
   const values = ['2016-01-01'];
   const { rows } = await db.query(query, values);
   res.send(rows);
