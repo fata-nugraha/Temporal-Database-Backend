ef0fdcb2376233a9d320ff50b8c950484e7a030a
Ft-N <ftnfata@gmail.com>
2020-11-25 08:15:05 +0700

    Change DB to postgre

--- src/database/index.js
+++ src/database/index.js
@@ -1,49 +1,5 @@
-require('dotenv').config()
-const { MongoClient } = require("mongodb");
-const databaseURI = process.env.DB_URI;
-
-async function exec(func){
-  const client = new MongoClient(databaseURI, {useUnifiedTopology: true});
-  try{
-    await client.connect();
-    const database = client.db("pdl2020");
-    const collection = database.collection("hotel");
-    await func(collection);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.close();
-  }
-}
-/*
-async function findOne(query, options){
-	return await exec((collection) => collection.find(query, options).limit(1));
-}
-
-async function find(query, options){
-  return await exec((collection) => collection.find(query, options));
-}
-
-async function insertOne(query){
-  return await exec((collection) => collection.insertOne(query));
-}
-
-async function updateOne(id, set){
-  return await exec((collection) => collection.updateOne(id, set));
-}
-
-async function max(firstStage, secondStage, thirdStage, fourthStage){
-  return await exec((collection) => collection.aggregate(firstStage, secondStage, thirdStage, fourthStage).limit(1));
-}
-
-async function aggregate(firstStage, secondStage, thirdStage, fourthStage){
-  return await exec((collection) => collection.aggregate(firstStage, secondStage, thirdStage, fourthStage));
-}
-
-async function preceed(firstStage, secondStage, thirdStage, fourthStage){
-  return await exec((collection) => collection.aggregate(firstStage, secondStage, thirdStage, fourthStage));
-}*/
-
+const { Pool } = require('pg')
+const pool = new Pool()
 module.exports = {
-	exec, /*findOne, find, insertOne, updateOne, max, aggregate, preceed*/
+  query: (text, params) => pool.query(text, params),
 }
