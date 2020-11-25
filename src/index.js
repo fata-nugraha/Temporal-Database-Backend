09e6a0d5cac91bfdf2569c9adb5b1ac9e1ec4abd
Ft-N <ftnfata@gmail.com>
2020-11-25 08:21:00 +0700

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
-  , app = express()
+  , cors = require('cors')
   , bodyParser = require('body-parser')
+  , app = express()
   , port = process.env.PORT || 3001
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 app.use(require('./api'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
